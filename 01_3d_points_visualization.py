0b57ed015c4cffbc3b8627c055e279371bb33f5b
minhncedutw <minhnc.edu.tw@gmail.com>
2018-09-28 10:22:32 +0800

    add custom color visualization

--- 01_3d_points_visualization.py
+++ 01_3d_points_visualization.py
@@ -35,22 +35,99 @@ from mayavi import mlab
 #==============================================================================
 # Constant Definitions
 #==============================================================================
-
+COLOR_BLACK = (0, 0, 0)
+COLOR_RED = (255, 0, 0)
+COLOR_GREEN = (0, 255, 0)
+COLOR_BLUE = (0, 0, 255)
+COLOR_YELLOW = (255, 255, 0)
+COLOR_PINK = (255, 0, 255)
+COLOR_CYAN = (0, 255, 255)
+COLOR_WHITE = (255, 255, 255)
 #==============================================================================
 # Function Definitions
 #==============================================================================
+def simple_visualization():
+    # View 3D points
+    x, y, z = np.random.random((3, 40))
+    color = (np.random.randint(0, 5, (40)) + 90) / 100  # create different label to be different color but similar size
+    mlab.points3d(x, y, z, color, scale_factor=0.1)
+    mlab.show()
+    return 0
 
+
+def visualize_different_color():
+    # Primitives
+    N = 200  # Number of points
+    ones = np.ones(N)
+    scalars = np.arange(N) # Key point: set an integer for each point
+    # scalars = np.random.randint(0, 5, N)
+
+    # Define color table (including alpha), which must be uint8 and [0,255]
+    colors = (np.random.random((N, 4)) * 255).astype(np.uint8)
+    colors[:, -1] = 255  # No transparency
+
+    # Define coordinates and points
+    x, y, z = colors[:, 0], colors[:, 1], colors[:, 2]  # Assign x, y, z values to match color
+    pts = mlab.quiver3d(x, y, z, ones, ones, ones, scalars=scalars, mode='sphere')  # Create points
+    pts.glyph.color_mode = 'color_by_scalar'  # Color by scalar
+
+    # Set look-up table and redraw
+    pts.module_manager.scalar_lut_manager.lut.table = colors
+    mlab.draw()
+    mlab.show()
+    return 0
+
+
+def custom_visulatilation():
+    def standard_color(transparent=255):
+        colors = np.array([COLOR_BLACK, COLOR_RED, COLOR_GREEN, COLOR_BLUE, COLOR_YELLOW, COLOR_PINK, COLOR_CYAN, COLOR_WHITE])
+        colors = np.c_[colors, np.ones((len(colors)))*transparent] # Add transparent value
+        print(colors)
+        return colors
+
+    def random_color(length, transparent=255):
+        # Define color table (including alpha), which must be uint8 and [0,255]
+        colors = (np.random.random((length, 4)) * transparent).astype(np.uint8)
+        colors[:, -1] = 255  # No transparency
+        return colors
+
+    def visualize(x, y, z, label):
+        N = len(x)
+
+        ones = np.ones(N)
+        pts = mlab.quiver3d(x, y, z, ones, ones, ones, scalars=label, mode='sphere') # Create points
+        pts.glyph.color_mode = 'color_by_scalar'  # Color by scalar
+
+        # colors = random_color(length=N)
+        colors = standard_color()
+
+        # Set look-up table and redraw
+        pts.module_manager.scalar_lut_manager.lut.table = colors
+        mlab.show()
+
+        return 0
+
+    # Primitives
+    N = 100  # Number of points
+
+    # Create point cloud
+    x= np.random.random((N))
+    y= np.random.random((N))
+    z= np.random.random((N))
+
+    # Create label
+    label = np.random.randint(low=0, high=8, size=(N))
+
+    visualize(x=x, y=y, z=z, label=label)
+
+    return 0
 #==============================================================================
 # Main function
 #==============================================================================
 def main(argv=None):
     print('Hello! This is Mayavi 3D points visualization Program')
 
-    # View 3D points
-    x, y, z = np.random.random((3, 40))
-    color = (np.random.randint(0, 5, (40)) + 90)/100 # create different label to be different color but similar size
-    mlab.points3d(x, y, z, color, scale_factor=0.1)
-    mlab.show()
+    custom_visulatilation()
 
 
 if __name__ == '__main__':
